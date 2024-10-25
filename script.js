c44e9e9a422a4cf6b423e8a226a66aec91c0cef3
Jovuto <153099429+Jovuto@users.noreply.github.com>
2024-10-25 14:24:45 +0100

    feat: add list of actions

--- script.js
+++ script.js
@@ -11,5 +11,20 @@ let playerTwo = {
 }
 
 let gameFlow = {
-    
+
 }
+
+/* 
+1. Create player 1 with a name
+2. Create player 2 with a name
+3. Print the board with who's turn it is above the board
+4. Player 1 selects a cell
+5. An O appears on the cell player 1 selects
+6. The game turn goes to player 2 with player 2's name above the board
+7. Player 2 selects a cell
+8. An X appears on the cell player 2 selects
+9. Play goes on until there is three in a row
+10. Once a three in a row is detected, a message appears saying "Player (1/2) wins!"
+11. The players are given an option to play again
+12. If selected, the game resets
+*/
